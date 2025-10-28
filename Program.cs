a2a045cf4cfb1ba0c790b6d181428efe7b326a4d
Krishant Acharya <krishant.acharya@aqore.com>
2025-10-28 12:11:17 +0545

    Initial commit: New .NET project with gitignore

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -20,3 +21,7 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+=======
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+>>>>>>> 8a62d22 (Initial commit: New .NET project with gitignore)
