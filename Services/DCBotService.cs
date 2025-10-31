a3e67f8c5821b49d8c1c36975a084f889de34908
CrackedArkade <acharyakrishant@gmail.com>
2025-10-31 12:22:50 +0545

    Added sound choice functionality

--- Services/DCBotService.cs
+++ Services/DCBotService.cs
@@ -6,6 +6,7 @@ using Discord;
 using Discord.WebSocket;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using System.Collections.Generic;
 
 
 public class DCBotService : IHostedService
@@ -30,13 +31,64 @@ public class DCBotService : IHostedService
     public async Task StartAsync(CancellationToken cancellationToken)
     {
         _client.Log += (msg) => { Console.WriteLine(msg.ToString()); return Task.CompletedTask; };
-        _client.Ready += () => { Console.WriteLine("Discord Bot is connected!"); return Task.CompletedTask; };
+
+
+        _client.Ready += Client_Ready;
+        _client.SlashCommandExecuted += SlashCommandHandler;
+
+
         _client.MessageReceived += OnMessageReceived;
 
         await _client.LoginAsync(TokenType.Bot, _configuration["Discord:Token"]);
         await _client.StartAsync();
 
         _bridge.SetDiscordClient(_client);
+    }
+
+    public async Task Client_Ready()
+    {
+        var soundChoices = new List<ApplicationCommandOptionChoiceProperties>
+        {
+            new ApplicationCommandOptionChoiceProperties() { Name = "Creeper Hiss", Value = "ENTITY_CREEPER_PRIMED" },
+            new ApplicationCommandOptionChoiceProperties() { Name = "Zombie Groan", Value = "ENTITY_ZOMBIE_AMBIENT" },
+            new ApplicationCommandOptionChoiceProperties() { Name = "Level Up", Value = "ENTITY_PLAYER_LEVELUP" },
+            new ApplicationCommandOptionChoiceProperties() { Name = "Ghast Scream", Value = "ENTITY_GHAST_SCREAM" },
+            new ApplicationCommandOptionChoiceProperties() { Name = "Villager No", Value = "ENTITY_VILLAGER_NO" },
+            new ApplicationCommandOptionChoiceProperties() { Name = "Anvil Land", Value = "BLOCK_ANVIL_LAND" }
+        };
+
+        var playSoundCommand = new SlashCommandBuilder()
+            .WithName("playsound")
+            .WithDescription("Play a Minecraft sound effect.")
+            .AddOption("player", ApplicationCommandOptionType.String, "The player to hear the sound.", isRequired: true)
+            .AddOption("sound", ApplicationCommandOptionType.String, "The sound effect to play.", isRequired: true, choices: soundChoices);
+
+        try
+        {
+            await _client.CreateGlobalApplicationCommandAsync(playSoundCommand.Build());
+            Console.WriteLine("Successfully registered the /playsound command.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error registering command: {ex.Message}");
+        }
+    }
+    private async Task SlashCommandHandler(SocketSlashCommand command)
+
+    {
+        if (command.Data.Name == "playsound")
+
+        {
+            string playerName = (string)command.Data.Options.First(o => o.Name == "player").Value;
+            string soundName = (string)command.Data.Options.First(o => o.Name == "sound").Value;
+            var soundRequest = new SoundRequest
+            {
+                PlayerName = playerName,
+                SoundName = soundName
+            };
+            _bridge.ToMinecraftSoundQueue.Enqueue(soundRequest);
+            await command.RespondAsync($"Sent the '{soundName}' sound to '{playerName}'!", ephemeral: true);
+        }
     }
 
     private Task OnMessageReceived(SocketMessage message)
@@ -85,7 +137,7 @@ public class DCBotService : IHostedService
 
         _bridge.ToMinecraftQueue.Enqueue(messageInfo);
         return Task.CompletedTask;
-    
+
     }
 
     public async Task StopAsync(CancellationToken cancellationToken)
